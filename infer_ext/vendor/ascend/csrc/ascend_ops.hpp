47dd6f231e23f822deabec0e1ea87cd8413be238
chenchiyu <chenchiyu@pjlab.org.cn>
2024-07-18 08:04:25 +0800

    fix(ascend): fix llama7B infer when batch_size=1

--- infer_ext/vendor/ascend/csrc/ascend_ops.hpp
+++ infer_ext/vendor/ascend/csrc/ascend_ops.hpp
@@ -12,10 +12,21 @@ at::Tensor npu_prompt_flash_attention_out(
     const at::Tensor &value, at::Tensor &attn_output,
     const c10::optional<at::Tensor> &padding_mask,
     const c10::optional<at::Tensor> &atten_mask,
-    c10::optional<at::IntArrayRef> actual_seq_lengths,
+    c10::OptionalIntArrayRef actual_seq_lengths,
     int64_t num_heads, double scale_value,
     int64_t pre_tokens, int64_t next_tokens,
     c10::string_view input_layout, int64_t num_key_value_heads);
+
+at::Tensor npu_incre_flash_attention_v4_out(
+    const at::Tensor &query, const at::Tensor &key, const at::Tensor &value, at::Tensor attn_output,
+    const c10::optional<at::Tensor> &padding_mask, const c10::optional<at::Tensor> &atten_mask,
+    c10::OptionalIntArrayRef actual_seq_lengths, const c10::optional<at::Tensor> &antiquant_scale,
+    const c10::optional<at::Tensor> &antiquant_offset, const c10::optional<at::Tensor> &block_table,
+    const c10::optional<at::Tensor> &dequant_scale1, const c10::optional<at::Tensor> &quant_scale1,
+    const c10::optional<at::Tensor> &dequant_scale2, const c10::optional<at::Tensor> &quant_scale2,
+    const c10::optional<at::Tensor> &quant_offset2, const c10::optional<at::Tensor> &kv_padding_size,
+    int64_t num_heads, double scale_value, c10::string_view input_layout, int64_t num_key_value_heads,
+    int64_t block_size, int64_t inner_precise);
 
 } // namespace ascend
 
--- infer_ext/vendor/ascend/csrc/init.cpp
+++ infer_ext/vendor/ascend/csrc/init.cpp
@@ -8,6 +8,12 @@ TORCH_LIBRARY(npu_ext, m) {
           "Tensor? padding_mask=None, Tensor? atten_mask=None, int[]? actual_seq_lengths=None, int num_heads=1, "
           "float scale_value=1.0, int pre_tokens=2147473647, int next_tokens=0, "
           "str input_layout=\"BSH\", int num_key_value_heads=0) -> Tensor(a!)");
+    m.def("npu_incre_flash_attention_v4_out(Tensor query, Tensor key, Tensor value, Tensor(a!) attn_output, *, "
+          "Tensor? padding_mask=None, Tensor? atten_mask=None, int[]? actual_seq_lengths=None, "
+          "Tensor? antiquant_scale=None, Tensor? antiquant_offset=None, Tensor? block_table=None, "
+          "Tensor? dequant_scale1=None, Tensor? quant_scale1=None, Tensor? dequant_scale2=None, Tensor? quant_scale2=None, "
+          "Tensor? quant_offset2=None, Tensor? kv_padding_size=None, int num_heads=1, float scale_value=1.0, "
+          "str input_layout=\"BSH\", int num_key_value_heads=0, int block_size=0, int inner_precise=1) -> Tensor(a!)");
 }
 
 } // namespace
@@ -16,6 +22,7 @@ namespace {
 
 TORCH_LIBRARY_IMPL(npu_ext, PrivateUse1, m) {
     m.impl("npu_prompt_flash_attention_out", TORCH_FN(infer_ext::ascend::npu_prompt_flash_attention_out));
+    m.impl("npu_incre_flash_attention_v4_out", TORCH_FN(infer_ext::ascend::npu_incre_flash_attention_v4_out));
 }
 
 } // namespace
