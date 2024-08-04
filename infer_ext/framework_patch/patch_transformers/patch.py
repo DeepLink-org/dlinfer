1ae30b8e77fabb5d71c2e89b24df69cbd92c5c81
jinminxi104 <jinminxi104@hotmail.com>
2024-08-04 17:31:08 +0800

    support get_class_in_module in more versions of transformers

--- infer_ext/framework_patch/patch_transformers/patch.py
+++ infer_ext/framework_patch/patch_transformers/patch.py
@@ -2,14 +2,14 @@ import transformers
 import inspect
 
 
-def apply_model_patches(module, name):
-    if name == 'transformers_modules.internlm2-chat-7b.modeling_internlm2':
+def apply_model_patches(module):
+    if module.__name__ == 'transformers_modules.internlm2-chat-7b.modeling_internlm2':
         from . import internlm2
         module.InternLM2RMSNorm.forward = internlm2.modeling_internlm2_InternLM2RMSNorm_forward
         module.InternLM2Attention.forward = internlm2.modeling_internlm2_InternLM2Attention_forward
         module.InternLM2ForCausalLM.prepare_inputs_for_generation = internlm2.modeling_internlm2_InternLM2ForCausalLM_prepare_inputs_for_generation
         transformers.cache_utils.DynamicCache.update = internlm2.transformers_cache_utils_dynamiccache_update
-    elif name == 'transformers_modules.InternVL-Chat-V1-5.modeling_internvl_chat':
+    elif module.__name__ == 'transformers_modules.InternVL-Chat-V1-5.modeling_internvl_chat':
         from . import internvl
         vit_module = inspect.getmodule(module.InternVisionModel)
         vit_module.InternAttention._naive_attn = internvl.InternAttention_naive_attn
