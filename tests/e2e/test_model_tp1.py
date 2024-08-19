381312c2a75d56ead15d482b1059218bf63abecf
Wei Tao <1136862851@qq.com>
2024-08-19 22:04:50 +0800

    format test code and add qwen test (#16)

--- tests/e2e/test_model_tp1.py
+++ tests/e2e/test_model_tp1.py
@@ -3,19 +3,20 @@ import pytest
 
 import infer_ext
 
-from ..utils.config_utils import  get_torch_model_list
-from ..utils.pipeline_chat import (assert_pipeline_chat_log,
-                                 run_pipeline_chat_test)
+from ..utils.config_utils import get_torch_model_list
+from ..utils.pipeline_chat import assert_pipeline_chat_log, run_pipeline_chat_test
 
 
-@pytest.mark.usefixtures('common_case_config')
+@pytest.mark.usefixtures("common_case_config")
 @pytest.mark.flaky(reruns=0)
-@pytest.mark.parametrize('model', get_torch_model_list(tp_num=1))
+@pytest.mark.parametrize("model", get_torch_model_list(tp_num=1))
 def test_pipeline_chat_pytorch_tp1(config, common_case_config, model):
-    p = Process(target=run_pipeline_chat_test,
-                args=(config, common_case_config, model, 'pytorch'))
+    p = Process(
+        target=run_pipeline_chat_test,
+        args=(config, common_case_config, model, "pytorch"),
+    )
     p.start()
     p.join()
 
     # assert script
-    assert_pipeline_chat_log(config, common_case_config, model, 'pytorch')
+    assert_pipeline_chat_log(config, common_case_config, model, "pytorch")
