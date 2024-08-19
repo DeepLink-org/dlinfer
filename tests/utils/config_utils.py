5bf1d34e0522b65221d8ed0720266f39e6d617d5
Wei Tao <1136862851@qq.com>
2024-08-19 22:04:50 +0800

    format test code and add qwen test (#16)

--- tests/utils/config_utils.py
+++ tests/utils/config_utils.py
@@ -5,26 +5,25 @@ from lmdeploy.model import MODELS
 
 TEST_DIR = os.environ.get("INFEREXT_TEST_DIR")
 
-def get_torch_model_list(tp_num: int = None, model_type: str = 'chat_model'):
+
+def get_torch_model_list(tp_num: int = None, model_type: str = "chat_model"):
     config = get_config()
-    case_list = config.get('pytorch_' + model_type)
+    case_list = config.get("pytorch_" + model_type)
     if tp_num is not None:
-        return [
-            item for item in case_list if get_tp_num(config, item) == tp_num
-        ]
+        return [item for item in case_list if get_tp_num(config, item) == tp_num]
     else:
         return case_list
 
 
 def get_config():
-    config_path = os.path.join(TEST_DIR + '/e2e/config.yaml')
+    config_path = os.path.join(TEST_DIR + "/e2e/config.yaml")
     with open(config_path) as f:
         config = yaml.load(f.read(), Loader=yaml.SafeLoader)
     return config
 
 
 def get_case_config():
-    case_path = os.path.join(TEST_DIR + '/e2e/prompt_case.yaml')
+    case_path = os.path.join(TEST_DIR + "/e2e/prompt_case.yaml")
     with open(case_path) as f:
         case_config = yaml.load(f.read(), Loader=yaml.SafeLoader)
     return case_config
@@ -32,14 +31,14 @@ def get_case_config():
 
 def get_tp_config(config, model, need_tp):
     tp_num = str(get_tp_num(config, model))
-    tp_info = ''
+    tp_info = ""
     if need_tp and tp_num is not None:
-        tp_info = '--tp ' + str(get_tp_num(config, model))
+        tp_info = "--tp " + str(get_tp_num(config, model))
     return tp_info
 
 
 def get_tp_num(config, model):
-    tp_config = config.get('tp_config')
+    tp_config = config.get("tp_config")
     tp_num = 1
     if tp_config is None:
         return None
@@ -50,9 +49,7 @@ def get_tp_num(config, model):
 
 
 def get_model_name(model):
-    model_names = [
-        'llama', 'llama2', 'llama3', 'internlm', 'internlm2'
-    ]
+    model_names = ["llama", "llama2", "llama3", "internlm", "internlm2"]
     model_names += list(MODELS.module_dict.keys())
     model_names.sort()
     model_name = _simple_model_name(model)
@@ -62,24 +59,23 @@ def get_model_name(model):
         return model_name
     if model_name in model_names:
         return model_name
-    if ('llama-2' in model_name):
-        return 'llama2'
-    if ('llama-3' in model_name):
-        return 'llama3'
-    if ('internlm2') in model_name:
-        return 'internlm2'
-    if len(model_name.split('-')) > 2 and '-'.join(
-            model_name.split('-')[0:2]) in model_names:
-        return '-'.join(model_name.split('-')[0:2])
-    return model_name.split('-')[0]
+    if "llama-2" in model_name:
+        return "llama2"
+    if "llama-3" in model_name:
+        return "llama3"
+    if ("internlm2") in model_name:
+        return "internlm2"
+    if (
+        len(model_name.split("-")) > 2
+        and "-".join(model_name.split("-")[0:2]) in model_names
+    ):
+        return "-".join(model_name.split("-")[0:2])
+    return model_name.split("-")[0]
 
 
 def _simple_model_name(model):
-    if '/' in model:
-        model_name = model.split('/')[1]
+    if "/" in model:
+        model_name = model.split("/")[1]
     else:
         model_name = model
-    model_name = model_name.replace('-inner-4bits', '')
-    model_name = model_name.replace('-inner-w8a8', '')
-    model_name = model_name.replace('-4bits', '')
     return model_name
