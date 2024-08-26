c9745f181bf8592ddcaf08b760596897795dafaf
CyCle1024 <chenchiyu@pjlab.org.cn>
2024-08-26 14:38:39 +0800

    ci: add format ci and format project (#23)

--- dlinfer/framework/transformers_ext/internvl.py
+++ dlinfer/framework/transformers_ext/internvl.py
@@ -1,6 +1,7 @@
 import torch
 import dlinfer.ops as ext_ops
 
+
 def InternAttention_naive_attn(self, x):
     B, N, C = x.shape
     qkv = self.qkv(x).reshape(B, N, 3, C).permute(2, 0, 1, 3)
@@ -10,14 +11,29 @@ def InternAttention_naive_attn(self, x):
         k = self.k_norm(k)
 
     attention_mask = None
-    start_loc = torch.tensor([N * i for i in range(B)],  device=q.device, dtype=torch.int64)
+    start_loc = torch.tensor(
+        [N * i for i in range(B)], device=q.device, dtype=torch.int64
+    )
     seq_len = torch.tensor([N for _ in range(B)], device=q.device, dtype=torch.int64)
-    ext_ops.prefill_attention(q, k, v, start_loc, seq_len, N, self.num_heads,
-                              self.num_heads, attn_mask=attention_mask, softmax_scale=None, alibi_slopes=None, attn_output=x)
+    ext_ops.prefill_attention(
+        q,
+        k,
+        v,
+        start_loc,
+        seq_len,
+        N,
+        self.num_heads,
+        self.num_heads,
+        attn_mask=attention_mask,
+        softmax_scale=None,
+        alibi_slopes=None,
+        attn_output=x,
+    )
 
     x = self.proj(x.reshape(B, N, C))
     x = self.proj_drop(x)
     return x
+
 
 def InternRMSNorm_forward(self, hidden_states):
     return ext_ops.rms_norm(hidden_states, self.weight, self.variance_epsilon)
