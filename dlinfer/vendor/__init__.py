565bf3327523c1f00a5ab5a9e1bd73f1b425ffb0
CyCle1024 <chenchiyu@pjlab.org.cn>
2024-08-26 17:26:01 +0800

    remove useless pytorch mock code (#28)

--- dlinfer/vendor/__init__.py
+++ dlinfer/vendor/__init__.py
@@ -7,7 +7,6 @@ import torch
 
 vendor_ops_registry = dict()
 vendor_is_initialized = False
-apply_vendor_pytorch_patch = None
 vendor_name_file = Path(__file__).parent / "vendor.yaml"
 with open(str(vendor_name_file), "r") as f:
     config = yaml.safe_load(f)
@@ -21,34 +20,6 @@ def import_vendor_module(vendor_name_str):
 
 
 def vendor_torch_init():
-    vendor_module = import_vendor_module(vendor_name)
-    global device_type, vendor_is_initialized, apply_vendor_pytorch_patch
+    import_vendor_module(vendor_name)
+    global vendor_is_initialized
     vendor_is_initialized = True
-    device_type = torch.device(0).type
-    apply_vendor_pytorch_patch = vendor_module.apply_vendor_pytorch_patch
-
-
-@lru_cache(1)
-def get_device_str():
-    if not vendor_is_initialized:
-        vendor_torch_init()
-    vendor_module = import_vendor_module(vendor_name)
-    return vendor_module.device_str
-
-
-@lru_cache(1)
-def get_comm_str():
-    if not vendor_is_initialized:
-        vendor_torch_init()
-    vendor_module = import_vendor_module(vendor_name)
-    return vendor_module.comm_str
-
-
-def load_extension_ops():
-    if not vendor_is_initialized:
-        vendor_torch_init()
-    extension_file_path = (
-        f"{str(Path(__file__).parent / vendor_name / vendor_name)}_extension.so"
-    )
-    if Path(extension_file_path).exists():
-        torch.ops.load_library(extension_file_path)
