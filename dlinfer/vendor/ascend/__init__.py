c86100dc78e3995f406cdd02a808222613a87c7e
chenchiyu <chenchiyu@pjlab.org.cn>
2024-08-26 15:56:12 +0800

    remove useless pytorch mock code

--- dlinfer/vendor/ascend/__init__.py
+++ dlinfer/vendor/ascend/__init__.py
@@ -1,2 +1,6 @@
-from .pytorch_patch import *
-from .torch_npu_ops import *
+from pathlib import Path
+
+import torch
+from . import pytorch_patch, torch_npu_ops
+
+torch.ops.load_library(str(Path(__file__).parent / "ascend_extension.so"))
