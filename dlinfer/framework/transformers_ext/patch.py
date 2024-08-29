90c9d2a545848312bf5a04290756c2e157cb230c
jinminxi104 <jinminxi104@hotmail.com>
2024-08-29 16:23:53 +0800

    fix module name in transformersLib

--- dlinfer/framework/transformers_ext/patch.py
+++ dlinfer/framework/transformers_ext/patch.py
@@ -2,9 +2,8 @@
 import transformers
 import inspect
 
-
 def apply_model_patches(module):
-    if module.__name__ == "transformers_modules.internlm2-chat-7b.modeling_internlm2":
+    if module.__name__.endswith(".modeling_internlm2"):
         from . import internlm2
 
         module.InternLM2RMSNorm.forward = (
@@ -19,13 +18,13 @@ def apply_model_patches(module):
         transformers.cache_utils.DynamicCache.update = (
             internlm2.transformers_cache_utils_dynamiccache_update
         )
-    elif module.__name__ == "transformers_modules.modeling_internvl_chat":
+    elif module.__name__.endswith(".modeling_internvl_chat"):
         from . import internvl
 
         vit_module = inspect.getmodule(module.InternVisionModel)
         vit_module.InternAttention._naive_attn = internvl.InternAttention_naive_attn
         vit_module.InternRMSNorm.forward = internvl.InternRMSNorm_forward
-    elif module.__name__ == "transformers_modules.cogvlm-chat.modeling_cogvlm":
+    elif module.__name__.endswith(".modeling_cogvlm"):
         from . import cogvlm
 
         # get parent module from another source code file
