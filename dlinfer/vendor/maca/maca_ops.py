dc33d1dead61e9d2fba171b2352d7852d5dfd154
zhoushenglong <zhoushenglong@pjlab.org.cn>
2024-09-19 06:13:42 +0000

    adjust maca ops to support restructed lmdeploy.

--- dlinfer/vendor/maca/maca_ops.py
+++ dlinfer/vendor/maca/maca_ops.py
@@ -10,7 +10,7 @@ from .maca_extension import ops as maca_ext_ops
 __all__ = [
     "add_rms_norm",
     "apply_rotary_pos_emb",
-    "context_attention",
+    "prefill_attention",
     "fill_kv_cache",
     "paged_decode_attention",
     "paged_prefill_attention",
@@ -39,14 +39,29 @@ def apply_rotary_pos_emb(
     position_ids: Optional[Tensor],
     cos_sin_cache: Optional[Tensor],
 ) -> Tuple[Tensor, Tensor]:
+    position_ids_1d = torch.arange(0, query.size(1), device=query.device)
+    query = query.flatten(-2, -1)
+    key = key.flatten(-2, -1)
+    cos = cos.squeeze(0).squeeze(1)
+    cos = cos[..., :cos.shape[-1] // 2]
+    sin = sin.squeeze(0).squeeze(1)
+    sin = sin[..., :sin.shape[-1] // 2]
+    cos_sin_cache = torch.cat((cos, sin), dim=-1)
+
     maca_ext_ops.rotary_embedding(
-        position_ids, query, key, cos_sin_cache.size(-1), cos_sin_cache, True
+        position_ids_1d,
+        query,
+        key,
+        cos_sin_cache.size(-1),
+        cos_sin_cache,
+        True
     )
+
     return query, key
 
 
 @register_ops(vendor_ops_registry)
-def context_attention(
+def prefill_attention(
     query: Tensor,
     key: Tensor,
     value: Tensor,
@@ -69,17 +84,23 @@ def context_attention(
     if softmax_scale is None:
         softmax_scale = float(1 / math.sqrt(key.size(-1)))
 
-    maca_ext_ops.flash_attn_varlen_fwd(
+    def make_cu_seqlens(seqlens):
+        cu_seqlens = seqlens.cumsum(0)
+        cu_zero = cu_seqlens.new_zeros(1)
+        cu_seqlens = torch.cat([cu_zero, cu_seqlens])
+        return cu_seqlens
+
+    cu_seqlens = make_cu_seqlens(q_seq_len).int().to(query.device)
+
+    attn_output = maca_ext_ops.flash_attn_varlen_fwd(
         query,
         key,
         value,
         attn_output,
-        q_start_loc,
-        q_start_loc,
-        q_seq_len,
-        q_seq_len,
+        cu_seqlens,
+        cu_seqlens,
         None,
-        None,
+        alibi_slopes,
         max_q_seq_len,
         max_q_seq_len,
         0.0,
@@ -91,7 +112,8 @@ def context_attention(
         False,
         None,
     )
-    return attn_output
+
+    return attn_output[0]
 
 
 @register_ops(vendor_ops_registry)
@@ -102,9 +124,12 @@ def fill_kv_cache(
     value_cache: Tensor,
     kv_indices: Tensor,
 ) -> Tuple[Tensor, Tensor]:
+    kv_indices = kv_indices.squeeze(-1)
+
     maca_ext_ops.reshape_and_cache_new(
         key, value, key_cache, value_cache, kv_indices, "auto"
     )
+
     return key_cache, value_cache
 
 
@@ -132,6 +157,10 @@ def paged_decode_attention(
     if softmax_scale is None:
         softmax_scale = float(1 / math.sqrt(query.size(-1)))
 
+    block_table = block_table.int()
+    kv_seq_len = kv_seq_len.int().to(query.device)
+
+    # import pdb; pdb.set_trace()
     maca_ext_ops.paged_attention_v1(
         attn_output,
         query,
@@ -146,6 +175,8 @@ def paged_decode_attention(
         None,
         "auto",
     )
+    # import pdb; pdb.set_trace()
+
     return attn_output
 
 
