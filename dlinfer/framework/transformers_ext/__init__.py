39cf66a435ead47b95fabe6293005511e7257100
Pan Daoxin <pandaoxin@pjlab.org.cn>
2024-10-12 06:09:56 +0000

    Support cogvlm xformers.

--- dlinfer/framework/transformers_ext/__init__.py
+++ dlinfer/framework/transformers_ext/__init__.py
@@ -4,12 +4,14 @@ import os, sys
 import typing
 from typing import Any, Dict, List, Optional, Union
 import transformers
+from .xformers import ops
 from .patch import apply_model_patches
 
 
 def patched_get_class_in_module(
     class_name: str, module_path: Union[str, os.PathLike]
 ) -> typing.Type:
+    sys.modules["xformers"] = xformers
     ret_class = transformers_get_class_in_module(class_name, module_path)
     apply_model_patches(importlib.import_module(ret_class.__module__))
     return ret_class
---
+++ dlinfer/framework/transformers_ext/xformers/ops.py
@@ -0,0 +1,3 @@
+# Copyright (c) 2024, DeepLink. All rights reserved.
+def fake():
+    pass
