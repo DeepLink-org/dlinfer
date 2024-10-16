d44dc6324566674875308e876eec429adc2baf33
yaofengchen <fengchenyao@foxmail.com>
2024-10-16 10:42:12 +0000

    refine quants module

--- dlinfer/framework/lmdeploy_ext/quants/__init__.py
+++ dlinfer/framework/lmdeploy_ext/quants/__init__.py
@@ -1,15 +1,10 @@
 # Copyright (c) 2024, DeepLink. All rights reserved.
 import importlib
-from pathlib import Path
 from functools import lru_cache
-import yaml
+from dlinfer.vendor import vendor_name
 
 
 awq_vendor = ["ascend"]
-vendor_name_file = Path(__file__).parent.parent.parent.parent / "vendor/vendor.yaml"
-with open(str(vendor_name_file), "r") as f:
-    config = yaml.safe_load(f)
-    vendor_name = config["vendor"]
 
 
 @lru_cache(1)
