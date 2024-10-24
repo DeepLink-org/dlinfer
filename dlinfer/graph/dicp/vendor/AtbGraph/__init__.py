fd24a4a95eb0389ad4a50e5ac8fccd2d2dbb822f
chenchiyu <chenchiyu@pjlab.org.cn>
2024-10-24 15:20:47 +0800

    feat: make cache_size_limit=256 in dynamo config

--- dlinfer/graph/dicp/vendor/AtbGraph/__init__.py
+++ dlinfer/graph/dicp/vendor/AtbGraph/__init__.py
@@ -1,4 +1,6 @@
 def atbgraph(gm, fake_input_tensor):
+    import torch._dynamo.config
+    torch._dynamo.config.cache_size_limit = 256
     from dlinfer.graph.dicp.dynamo_bridge.compile_fx import compile_fx
 
     return compile_fx(gm, fake_input_tensor, "atbgraph")
