f397e0846a98e80e5f6413e0cabb3e699493fc8f
JackWeiw <51255903105@stu.ecnu.edu.cn>
2024-10-28 16:39:09 +0800

    ci:fix default eager mode

--- tests/test_lmdeploy/e2e/test_model_tp1.py
+++ tests/test_lmdeploy/e2e/test_model_tp1.py
@@ -17,10 +17,10 @@ from test_lmdeploy.utils.pipeline_chat import (
 @pytest.mark.flaky(reruns=0)
 @pytest.mark.lmdeploy
 @pytest.mark.parametrize("model", get_torch_model_list(tp_num=1))
-def test_pipeline_chat_pytorch_tp1_ascend(config, common_case_config, model):
+def test_pipeline_chat_pytorch_tp1_ascend_eager(config, common_case_config, model):
     p = Process(
         target=run_pipeline_chat_test,
-        args=(config, common_case_config, model, "ascend"),
+        args=(config, common_case_config, model, "ascend", True),
     )
     p.start()
     p.join()
@@ -32,10 +32,10 @@ def test_pipeline_chat_pytorch_tp1_ascend(config, common_case_config, model):
 @pytest.mark.flaky(reruns=0)
 @pytest.mark.lmdeploy
 @pytest.mark.parametrize("model", get_torch_model_list(tp_num=1, model_type="vl_model"))
-def test_pipeline_vl_pytorch_tp1_ascend(config, model):
+def test_pipeline_vl_pytorch_tp1_ascend_eager(config, model):
     p = Process(target=run_pipeline_vl_chat_test, args=(config, model, "ascend"))
     p.start()
     p.join()
 
     # assert script
-    assert_pipeline_vl_chat_log(config, model, "ascend")
+    assert_pipeline_vl_chat_log(config, model, "ascend", True)
