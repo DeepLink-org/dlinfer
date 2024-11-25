3c95f375f8da733aca683fe166e468ed8e8956c3
tangzhiyi11 <tangzhiyi@pjlab.org.cn>
2024-11-25 01:58:32 +0000

    refacotr aclnn ops register

--- dlinfer/graph/dicp/vendor/AtbGraph/codegen/runtime/ops/operation_creator.h
+++ dlinfer/graph/dicp/vendor/AtbGraph/codegen/runtime/ops/operation_creator.h
@@ -2,67 +2,27 @@
 
 #include <nlohmann/json.hpp>
 
+#include <iostream>
+#include <unordered_map>
 #include <string>
 
-#include "all_ops.h"
 #include "atb/operation.h"
 
 namespace dicp {
 
 using OperationCreateFunc = std::function<atb::Operation*(const nlohmann::json& paramJson)>;
 
-static std::map<std::string, OperationCreateFunc> g_funcMap = {
-    {"LinearOperation", &LinearOperationCreate},
-    {"ElewiseOperation", &ElewiseOperationCreate},
-    {"RmsNormOperation", &RmsNormOperationCreate},
-    {"RopeOperation", &RopeOperationCreate},
-    {"SelfAttentionOperation", &SelfAttentionOperationCreate},
-    {"ReshapeAndCacheOperation", &ReshapeAndCacheOperationCreate},
-    {"PagedAttentionOperation", &PagedAttentionOperationCreate},
-    {"AddRmsNormOperation", &AclNnAddRmsNormOperationCreate},
-    {"TransposeOperation", &TransposeOperationCreate},
-    {"SplitOperation", &SplitOperationCreate},
-    {"SplitWithSizeOperation", &AclNnSplitWithSizeOperationCreate},
-    {"ActivationOperation", &ActivationOperationCreate},
-    {"AclNnCatOperation", &AclNnCatOperationCreate},
-    {"AclNnBatchMatMulOperation", &AclNnBatchMatMulOperationCreate},
-    {"GatherOperation", &GatherOperationCreate},
-    {"AclNnPermuteOperation", &AclNnPermuteOperationCreate},
-    {"AclNnCastOperation", &AclNnCastOperationCreate},
-    {"AclNnDivOperation", &AclNnDivOperationCreate},
-    {"AclNnDivsOperation", &AclNnDivsOperationCreate},
-    {"AclNnAddOperation", &AclNnAddOperationCreate},
-    {"AclNnAddsOperation", &AclNnAddsOperationCreate},
-    {"AclNnMulOperation", &AclNnMulOperationCreate},
-    {"AclNnMulsOperation", &AclNnMulsOperationCreate},
-    {"AclNnSubOperation", &AclNnSubOperationCreate},
-    {"AclNnSubsOperation", &AclNnSubsOperationCreate},
-    {"AclNnPowTensorScalarOperation", &AclNnPowTensorScalarOperationCreate},
-    {"AclNnPowTensorTensorOperation", &AclNnPowTensorTensorOperationCreate},
-    {"AclNnMaxOperation", &AclNnMaxOperationCreate},
-    {"AclNnReciprocalOperation", &AclNnReciprocalOperationCreate},
-    {"AclNnGatherOperation", &AclNnGatherOperationCreate},
-    {"AclNnGeScalarOperation", &AclNnGeScalarOperationCreate},
-    {"AclNnGtScalarOperation", &AclNnGtScalarOperationCreate},
-    {"AclNnSWhereOperation", &AclNnSWhereOperationCreate},
-    {"AclNnArangeOperation", &AclNnArangeOperationCreate},
-    {"LinearParallelOperation", &LinearParallelOperationCreate},
-    {"SoftmaxOperation", &SoftmaxOperationCreate},
-    {"SortOperation", &SortOperationCreate},
-    {"SliceOperation", &SliceOperationCreate},
-    {"AclNnIndexSelectOperation", &AclNnIndexSelectOperationCreate},
-    {"AclNnSliceOperation", &AclNnSliceOperationCreate},
-    {"AclNnSoftmaxOperation", &AclNnSoftmaxOperationCreate},
-    {"AllReduceOperation", &AllReduceOperationCreate},
-    {"AclNnTopkOperation", &AclNnTopkOperationCreate},
-    {"AclNnInplaceDivOperation", &AclNnInplaceDivOperationCreate},
-    {"AclNnInplaceScatterOperation", &AclNnInplaceScatterOperationCreate},
-    {"AclNnExpandOperation", &AclNnExpandOperationCreate},
-    {"CustomViewOperation", &CustomViewOperationCreate},
-    {"CustomUnsqueezeOperation", &CustomUnsqueezeOperationCreate},
-    {"CustomSqueezeOperation", &CustomSqueezeOperationCreate},
-    {"CustomScalarTensorOperation", &CustomScalarTensorOperationCreate},
+std::unordered_map<std::string, OperationCreateFunc>& getGlobalFuncMap();
+
+struct RegisterOp {
+    RegisterOp(const std::string& name, OperationCreateFunc func) {
+        std::cout << "################# in RegisterOp: name: " << name << std::endl;
+        getGlobalFuncMap()[name] = func;
+    }
 };
+
+#define REGISTER_OPERATION(OpName, CreateFunc) \
+    static RegisterOp reg##OpName(#OpName, CreateFunc);
 
 atb::Operation* CreateOperation(const std::string& opName, const nlohmann::json& paramJson);
 
