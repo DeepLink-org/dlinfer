a7bf246ec0384b02e6535d0482f6125e4b8415a8
JackWeiw <51255903105@stu.ecnu.edu.cn>
2024-12-13 13:52:24 +0800

    [camb]use new mock api

--- dlinfer/vendor/camb/pytorch_patch.py
+++ dlinfer/vendor/camb/pytorch_patch.py
@@ -2,6 +2,6 @@ import torch
 import torch_mlu
 
 origin_torch_compile = torch.compile
-from torch_mlu.utils.model_transfer import transfer
+from torch_mlu.utils.gpu_migration import migration
 
 torch.compile = origin_torch_compile
