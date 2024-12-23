f10ff33b5ac4b3564420cc90958ad05fdf2bd7eb
JackWeiw <51255903105@stu.ecnu.edu.cn>
2024-12-23 14:05:31 +0800

    [camb]add patch to lmdeploy

---
+++ dlinfer/framework/lmdeploy_ext/device/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024, DeepLink. All rights reserved.
+import importlib
+from functools import lru_cache
+from dlinfer.vendor import vendor_name
+
+
+vendor = ["camb"]
+
+
+@lru_cache(1)
+def import_vendor_module(vendor_name_str):
+    if vendor_name_str in vendor:
+        importlib.import_module(f".{vendor_name_str}", __package__)
+
+
+def vendor_device_init():
+    import_vendor_module(vendor_name)
+
+
+vendor_device_init()
