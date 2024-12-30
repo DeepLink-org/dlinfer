48ac1780f84f94fd70d7b11d2fe793f7abe3769d
CyCle1024 <chenchiyu@pjlab.org.cn>
2024-12-30 15:55:28 +0800

    reopen ascend ci (#145)

--- tests/test_lmdeploy/utils/config_utils.py
+++ tests/test_lmdeploy/utils/config_utils.py
@@ -27,15 +27,37 @@ def get_torch_model_list(
         return case_list
 
 
+def get_env_with_replace_home(env_str, home_str):
+    env_var = os.getenv(env_str)
+    if r"${HOME}" in env_var:
+        env_var = env_var.replace(r"${HOME}", home_str)
+    if "$HOME" in env_var:
+        env_var = env_var.replace("$HOME", home_str)
+    return env_var
+
+
 def get_config():
-    config_path = os.path.join(TEST_DIR + "/test_lmdeploy/e2e/config.yaml")
+    home_dir = os.getenv("HOME")
+    model_path = get_env_with_replace_home("TEST_LMDEPLOY_E2E_MODEL_PATH", home_dir)
+    log_path = get_env_with_replace_home("TEST_LMDEPLOY_E2E_LOG_PATH", home_dir)
+    local_pic1 = get_env_with_replace_home("TEST_LMDEPLOY_E2E_LOCAL_PIC1", home_dir)
+    local_pic2 = get_env_with_replace_home("TEST_LMDEPLOY_E2E_LOCAL_PIC2", home_dir)
+    assert (model_path is not None) and (log_path is not None)
+    config_path = os.path.join(TEST_DIR, "test_lmdeploy/e2e/config.yaml")
     with open(config_path) as f:
         config = yaml.load(f.read(), Loader=yaml.SafeLoader)
+    config["model_path"] = model_path
+    config["log_path"] = log_path
+    os.makedirs(log_path, exist_ok=True)
+    if local_pic1:
+        config["LOCAL_PIC1"] = local_pic1
+    if local_pic2:
+        config["LOCAL_PIC2"] = local_pic2
     return config
 
 
 def get_case_config():
-    case_path = os.path.join(TEST_DIR + "/test_lmdeploy/e2e/prompt_case.yaml")
+    case_path = os.path.join(TEST_DIR, "test_lmdeploy/e2e/prompt_case.yaml")
     with open(case_path) as f:
         case_config = yaml.load(f.read(), Loader=yaml.SafeLoader)
     return case_config
