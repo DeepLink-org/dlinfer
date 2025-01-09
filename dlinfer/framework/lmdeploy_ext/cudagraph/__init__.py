c3f7e3f5e35f47774362e4f17cdd50c44373e6b5
JackWeiw <51255903105@stu.ecnu.edu.cn>
2025-01-09 11:25:18 +0800

    [camb]add cudagraph patch lmdeploy

---
+++ dlinfer/framework/lmdeploy_ext/cudagraph/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024, DeepLink. All rights reserved.
+import importlib
+from functools import lru_cache
+from dlinfer.vendor import vendor_name
+
+
+graph_vendor = ["camb", "maca"]
+
+
+@lru_cache(1)
+def import_vendor_module(vendor_name_str):
+    if vendor_name_str in graph_vendor:
+        importlib.import_module(f".{vendor_name_str}_cudagraph", __package__)
+
+
+def vendor_graph_init():
+    import_vendor_module(vendor_name)
+
+
+vendor_graph_init()
