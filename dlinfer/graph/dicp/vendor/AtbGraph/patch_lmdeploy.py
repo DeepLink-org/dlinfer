37c51ddda92de0ef5f786d7a43ec10a920c4d578
yaofengchen <fengchenyao@foxmail.com>
2025-01-16 11:52:23 +0000

    format code

--- dlinfer/graph/dicp/vendor/AtbGraph/patch_lmdeploy.py
+++ dlinfer/graph/dicp/vendor/AtbGraph/patch_lmdeploy.py
@@ -3,11 +3,13 @@ from typing import List, Callable
 from lmdeploy.pytorch.models import qwen2_vl
 
 
-def dlinfer_apply_mrope_selection(hidden_states: torch.Tensor,
-                           mrope_position_ids: torch.Tensor,
-                           mrope_section: List[int],
-                           position_ids: torch.Tensor,
-                           rotary_emb_func: Callable):
+def dlinfer_apply_mrope_selection(
+    hidden_states: torch.Tensor,
+    mrope_position_ids: torch.Tensor,
+    mrope_section: List[int],
+    position_ids: torch.Tensor,
+    rotary_emb_func: Callable,
+):
     _mrope_position_ids = torch.clone(mrope_position_ids)
     cos, sin = rotary_emb_func(hidden_states, _mrope_position_ids)
 
