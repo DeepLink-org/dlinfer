a7e141f147c4861169eaeeb8b8fa43dcb75337e9
JackWeiw <51255903105@stu.ecnu.edu.cn>
2025-02-27 10:27:06 +0800

    [camb]update smooth to next pow of 2

--- dlinfer/vendor/camb/__init__.py
+++ dlinfer/vendor/camb/__init__.py
@@ -3,4 +3,22 @@ import torch
 from . import pytorch_patch, camb_ops
 
 # TODO. weitao: camb torch-mlu-ops-v1.2.0 per_token_smooth_quantize need smooth_vec
-SMOOTH_VEC = torch.ones(8000, dtype=torch.float32, device="mlu")
+SMOOTH_VEC = torch.ones(8192, dtype=torch.float32, device='mlu')
+
+def next_power_of_2(n: int):
+    """Return the smallest power of 2 greater than or equal to n."""
+    n -= 1
+    n |= n >> 1
+    n |= n >> 2
+    n |= n >> 4
+    n |= n >> 8
+    n |= n >> 16
+    n |= n >> 32
+    n += 1
+    return n
+
+def update_smooth(length):
+    global SMOOTH_VEC
+    if length > SMOOTH_VEC.shape[0]:
+        SMOOTH_VEC = torch.ones(next_power_of_2(length), dtype=torch.float32, device='mlu')        
+    return SMOOTH_VEC
