2ba923e1677bd75cd6b2b885992fe8328256710d
jinminxi104 <jinminxi104@hotmail.com>
2025-11-04 19:32:40 +0800

    Fix dep (#270)

--- scripts/build_wheel_allpy.sh
+++ scripts/build_wheel_allpy.sh
@@ -6,12 +6,12 @@ eval "$(conda shell.bash hook)"
 REPO_ROOT=$(cd $(dirname $(dirname $0)); pwd)
 cd ${REPO_ROOT}
 
-PY_VERSION_LIST=("3.8" "3.9" "3.10")
+PY_VERSION_LIST=("3.10" "3.11")
 for PY_VERSION in ${PY_VERSION_LIST[@]}; do
     echo start building wheels for python${PY_VERSION}
     PY_VERSION_NAME=${PY_VERSION/./}
     ENV_NAME=dlinfer_build_py${PY_VERSION_NAME}
-    conda env remove -n ${ENV_NAME} -y
+    # conda env remove -n ${ENV_NAME} -y
     conda create -n ${ENV_NAME} python=${PY_VERSION} -y
     conda activate ${ENV_NAME}
     pip install -U build 
