fix qwenvl precision

dlinfer/vendor/maca/maca_ops.py
@@ -12,6 +12,26 @@ from dlinfer.utils.type_annotation import Tensor, Optional, Sequence, Tuple
 
 from .fused_moe import fused_experts
 from .maca_extension import ops as maca_ext_ops
+from mcoplib import lmdeploy as mcoplib_ops
+from mcoplib import op as op_origin
+import mcoplib._C
+import mcoplib._moe_C
+
+env_value = os.getenv("MACA_LMDEPLOY_MCOPLIB_OPS", "true")
+USE_MCOPLIB_OPS = env_value.lower() in ("true", "1", "yes", "on")
+
+# Select the ops library based on environment variable
+if USE_MCOPLIB_OPS:
+    print(f"====>{USE_MCOPLIB_OPS}")
+    ops = mcoplib_ops
+    ops_name = "mcoplib_ops"
+else:
+    ops = maca_ext_ops
+    ops_name = "maca_ext_ops"
+
+# Print environment variable value and selected ops library
+print(f"[DLInfer] MACA_LMDEPLOY_MCOPLIB_OPS environment variable: {env_value} USE_MCOPLIB_OPS:{USE_MCOPLIB_OPS}")
+print(f"[DLInfer] Using ops library: {ops_name}")
 
 __all__ = [
     "add_rms_norm",
@@ -58,7 +78,10 @@ def add_rms_norm(
     weight: Tensor,
     epsilon: float,
 ) -> Tuple[Tensor, Tensor]:
-    maca_ext_ops.fused_add_rms_norm(hidden_states, residual, weight, epsilon)
+    if USE_MCOPLIB_OPS:
+        torch.ops._C.fused_add_rms_norm(hidden_states, residual, weight, epsilon)
+    else:
+        ops.fused_add_rms_norm(hidden_states, residual, weight, epsilon)
     return hidden_states, residual
 
 
@@ -76,16 +99,27 @@ def apply_rotary_pos_emb(
     query = query.flatten(-2, -1)
     key = key.flatten(-2, -1)
     rot_dim = cos.size(-1)
+    if USE_MCOPLIB_OPS:
+       ops.lmdeploy_rotary_embedding(
+            position_ids_1d,
+            query,
+            key,
+            head_size,
+            cos.view(-1, rot_dim),
+            sin.view(-1, rot_dim),
+            True,
+            )
+    else:
+        ops.rotary_embedding(
+            position_ids_1d,
+            query,
+            key,
+            head_size,
+            cos.view(-1, rot_dim),
+            sin.view(-1, rot_dim),
+            True,
+            )
 
-    maca_ext_ops.rotary_embedding(
-        position_ids_1d,
-        query,
-        key,
-        head_size,
-        cos.view(-1, rot_dim),
-        sin.view(-1, rot_dim),
-        True,
-    )
     return query, key
 
 
@@ -150,18 +184,22 @@ def prefill_attention(
         return attn_output
 
     # for cogvlm vl part.
-    if q_start_loc.size(0) == q_seq_len.size(0):
+    if query.size(-2) != num_q_heads:
         causal = False
-        #head_dim = query.size(-1) // num_q_heads
-        #query = query.view(-1, num_q_heads, head_dim)
-        #key = key.view(-1, num_kv_heads, head_dim)
-        #value = value.view(-1, num_kv_heads, head_dim)
-        #q_start_loc = torch.tensor(
-        #    [0, q_seq_len.size(0) + 1], dtype=torch.int32, device=query.device
-        #)
-        q_start_loc = torch.cat((torch.tensor([0], dtype=torch.int32, device=query.device), q_seq_len.cumsum(0).to(torch.int32)), dim=0)
-        #softmax_scale = float(1 / math.sqrt(head_dim))
+        head_dim = query.size(-1) // num_q_heads
+        query = query.view(-1, num_q_heads, head_dim)
+        key = key.view(-1, num_kv_heads, head_dim)
+        value = value.view(-1, num_kv_heads, head_dim)
+        q_start_loc = torch.tensor(
+            [0, q_seq_len], dtype=torch.int32, device=query.device
+        )
+        softmax_scale = float(1 / math.sqrt(head_dim))
 
+    # for qwen vl part.
+    if q_start_loc.shape[0] == q_seq_len.shape[0]:
+        causal = False
+        q_start_loc = torch.cat([q_start_loc, q_seq_len.sum().to(torch.int32).unsqueeze(0)])
+    
     output = flash_attn_varlen_func(
         query,
         key,
@@ -174,6 +212,7 @@ def prefill_attention(
         causal=causal,
         window_size=(-1, -1),
     )
+    attn_output.copy_(output)
     return output
 
 
@@ -201,16 +240,15 @@ def fill_kv_cache(
     quant_bits: int,
 ) -> Tuple[Tensor, Tensor]:
     kv_indices = kv_indices.squeeze(-1)
-    maca_ext_ops.reshape_and_cache_flash(
-        key,
-        value,
-        key_cache,
-        value_cache,
-        kv_indices,
-        "auto",
-        torch.tensor(1.0),
-        torch.tensor(1.0),
-    )
+    k_scale = torch.tensor(1.0)
+    v_scale = torch.tensor(1.0)
+    
+    if USE_MCOPLIB_OPS:
+        torch.ops._C_cache_ops.reshape_and_cache_flash(key, value, key_cache, value_cache, kv_indices, "auto", k_scale, v_scale)
+    else:
+        ops.reshape_and_cache_flash(
+            key, value, key_cache, value_cache, kv_indices, "auto", k_scale, v_scale
+        )
     return key_cache, value_cache
 
 
@@ -239,8 +277,6 @@ def paged_decode_attention(
 
     num_kv_heads = value_cache.size(1)
     block_size = value_cache.size(-2)
-    output = torch.empty_like(query)
-
     is_mla = query.size(-1) == 576
 
     if is_mla:
@@ -348,8 +384,10 @@ def rms_norm(
     hidden_states = hidden_states.to(torch.float32)
     weight = weight.to(torch.float32)
     output = torch.empty_like(hidden_states)
-    maca_ext_ops.rms_norm(output, hidden_states, weight, epsilon)
-
+    if USE_MCOPLIB_OPS:
+       op_origin.rms_norm(output, hidden_states, weight, epsilon, None, None,False)
+    else:
+        ops.rms_norm(output, hidden_states, weight, epsilon)
     return output.to(input_dtype)
 
 
@@ -367,12 +405,20 @@ def moe_gating_topk_softmax(
 
     token_expert_indicies = torch.empty_like(topk_ids)
 
-    maca_ext_ops.topk_softmax(
-        topk_weights,
-        topk_ids,
-        token_expert_indicies,
-        router_logits.float(),
-    )
+    if USE_MCOPLIB_OPS:
+        torch.ops._moe_C.topk_softmax(
+            topk_weights,
+            topk_ids,
+            token_expert_indicies,
+            router_logits.float()
+            )
+    else:
+        ops.topk_softmax(
+            topk_weights,
+            topk_ids,
+            token_expert_indicies,
+            router_logits.float(),
+        )
 
     del token_expert_indicies  # Not used. Will be used in the future.
 
@@ -389,7 +435,8 @@ def silu_and_mul(x: Tensor, dim: int = -1) -> Tensor:
     d = x.shape[-1] // 2
     output_shape = x.shape[:-1] + (d,)
     out = torch.empty(output_shape, dtype=x.dtype, device=x.device)
-    maca_ext_ops.silu_and_mul(out, x)
+    torch.ops._C.silu_and_mul(out, x)
+    #ops.silu_and_mul(out, x)
     return out
 
 