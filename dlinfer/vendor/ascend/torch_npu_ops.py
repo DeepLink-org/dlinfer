e8411e42411f8d97e21cf029f45de7f18f7654b7
tangzhiyi <tangzhiyi@pjlab.org.cn>
2025-11-24 06:53:32 +0000

    adjust fill kv cache op

--- dlinfer/vendor/ascend/torch_npu_ops.py
+++ dlinfer/vendor/ascend/torch_npu_ops.py
@@ -156,28 +156,25 @@ def fill_kv_cache(
     v_scales_zeros: Sequence[Optional[Tensor]],
     quant_bits: int,
 ) -> Tuple[Tensor, Tensor]:
+    _, head, dim = key.shape
+    block_num, block_size = key_cache.shape[:2]
+    block_total = block_num * block_size
     # only support contiguous k,v
     key = key.contiguous()
     value = value.contiguous()
-
+    kv_indices = kv_indices.view(-1, 1)
     if quant_bits == 8:
-
         def quant_int8(x, x_scale, x_offset):
             quantized = (
                 ((x / x_scale) - x_offset).round().clamp(-128, 127).to(torch.int8)
             )
             return quantized
-
         key = quant_int8(key, k_scales_zeros[0], k_scales_zeros[1])
         value = quant_int8(value, v_scales_zeros[0], v_scales_zeros[1])
-
-    torch.ops.atb._npu_reshape_and_cache(
-        key=key,
-        value=value,
-        key_cache=key_cache,
-        value_cache=value_cache,
-        slot_indices=kv_indices.to(torch.int32),
-    )
+    key_cache_reshaped = key_cache.view(block_total, head, dim)
+    value_cache_reshaped = value_cache.view(block_total, head, dim)
+    torch.ops.npu.npu_scatter_nd_update_(key_cache_reshaped, kv_indices, key)
+    torch.ops.npu.npu_scatter_nd_update_(value_cache_reshaped, kv_indices, value)
     return key_cache, value_cache
 
 
