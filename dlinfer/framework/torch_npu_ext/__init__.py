5c4747371b6bd3f39a71656c74e99e3429f259d1
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2025-11-26 14:35:25 +0800

    fix qwen3-30b lcb-code score accuracy (#276)

--- dlinfer/framework/torch_npu_ext/__init__.py
+++ dlinfer/framework/torch_npu_ext/__init__.py
@@ -1,11 +1,12 @@
 # Copyright (c) 2024, DeepLink. All rights reserved.
 from dlinfer.vendor import vendor_name
-from .aclgraph import appy_patch
 
 
 def torch_npu_ext_init():
     """Initialize torch npu extensions."""
     if vendor_name == "ascend":
+        from .aclgraph import appy_patch
+
         appy_patch()
 
 
