95b32a318083a20a817adc1619ae264fa60613ae
yaofengchen <67218893+yao-fengchen@users.noreply.github.com>
2025-11-30 22:52:16 +0800

    fix dummy case error (#280)

--- dlinfer/framework/lmdeploy_ext/device/__init__.py
+++ dlinfer/framework/lmdeploy_ext/device/__init__.py
@@ -3,10 +3,96 @@ import importlib
 import torch
 from functools import lru_cache
 from dlinfer.vendor import vendor_name
-from lmdeploy.pytorch import models
 
 
 vendor = ["camb", "ascend"]
+
+
+def patch_StepContext_new():
+    from typing import Literal, List
+    from lmdeploy.pytorch.model_inputs import ModelInputs, ModelConfig, StepContext
+    from lmdeploy.pytorch.backends import get_backend
+
+    def new(
+        cls,
+        inputs: ModelInputs,
+        model_config: ModelConfig,
+        kv_caches: List = None,
+        state_caches: List = None,
+        kv_quant_policy: Literal[0, 4, 8] = 0,
+    ):
+        """Build step context.
+
+        Args:
+            inputs (ModelInputs): packaged model inputs.
+            device (str): The device of the tensors.
+        """
+        q_seqlens = inputs.seq_length
+        history_seqlens = inputs.history_lengths
+
+        input_multimodals = None
+        if inputs.vision_inputs is not None:
+            input_multimodals = inputs.vision_inputs.input_multimodals
+
+        # for vlm
+        input_embeddings, input_embedding_indexing = None, None
+        if (
+            inputs.vision_inputs is not None
+            and inputs.vision_inputs.input_embeddings is not None
+        ):
+            input_embeddings, input_embedding_indexing = (
+                inputs.vision_inputs.get_inputs(history_seqlens, q_seqlens)
+            )
+
+        # position ids
+        attention_mask, position_ids = cls.get_mask_and_position_ids(inputs)
+        q_start_loc = q_seqlens.cumsum(0) - q_seqlens
+
+        # cross
+        cross_seqlens = inputs.cross_length
+        cross_kv_seqlens = None
+        if inputs.cross_length is not None:
+            cross_kv_seqlens = inputs.cross_length + inputs.history_cross_length
+
+        # seq_len + history_length
+        kv_seqlens = q_seqlens + history_seqlens
+        kv_seqlens -= inputs.num_ignored_history
+        # if inputs.is_dummy:
+        #     kv_seqlens = torch.zeros_like(kv_seqlens)
+
+        ret = StepContext(
+            input_ids=inputs.input_ids,
+            model_config=model_config,
+            block_offsets=inputs.block_offsets,
+            position_ids=position_ids,
+            input_embeddings=input_embeddings,
+            input_embedding_indexing=input_embedding_indexing,
+            input_multimodals=input_multimodals,
+            attention_mask=attention_mask,
+            q_seqlens=q_seqlens,
+            kv_seqlens=kv_seqlens,
+            q_start_loc=q_start_loc,
+            kv_caches=kv_caches,
+            is_decoding=inputs.is_decoding,
+            sum_kv_seqlen=inputs.sum_kv_seqlen,
+            max_kv_seqlen=inputs.max_kv_seqlen,
+            local_adapter_ids=inputs.local_adapter_ids,
+            vision_inputs=inputs.vision_inputs,
+            kv_quant_policy=kv_quant_policy,
+            model_metas=inputs.model_metas,
+            cross_seqlens=cross_seqlens,
+            cross_kv_seqlens=cross_kv_seqlens,
+            dp_meta=inputs.dp_meta,
+            enable_microbatch=inputs.enable_microbatch,
+            state_caches=state_caches,
+            state_offsets=inputs.state_offsets,
+            target_hidden_states=inputs.target_hidden_states,
+        )
+
+        ret = get_backend().update_step_context(ret)
+        return ret
+
+    StepContext.new = classmethod(new)
 
 
 def fake_torch_compile(dynamic=False):
@@ -113,6 +199,7 @@ def vendor_device_init():
     import_vendor_module(vendor_name)
     patch_compiled_func()
     patch_async_sampling_logits()
+    patch_StepContext_new()
 
 
 vendor_device_init()
