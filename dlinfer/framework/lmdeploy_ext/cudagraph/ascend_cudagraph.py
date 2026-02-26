90763f1e81ba84e7653fea372176b0d0b134f6c7
WangQing <2917021186@qq.com>
2026-02-26 08:30:30 +0000

    fix

--- dlinfer/framework/lmdeploy_ext/cudagraph/ascend_cudagraph.py
+++ dlinfer/framework/lmdeploy_ext/cudagraph/ascend_cudagraph.py
@@ -109,7 +109,9 @@ def AscendCudaGraphMixin_fill_buffers_cudagraph(
     input_buffers["kv_start_indices"][:batch_size] = kv_start_indices
     if x_active_mask is not None:
         input_buffers["x_active_mask"][:batch_size] = x_active_mask
-    input_buffers["q_start_loc"][:batch_size+1] = q_start_loc
+        
+    if 'state_ids' in kwargs:
+        input_buffers["q_start_loc"][:batch_size+1] = q_start_loc
 
     if inputs_embeds is not None:
         emb_size = inputs_embeds.size(-1)
