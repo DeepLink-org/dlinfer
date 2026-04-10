2ac6454d8a0f12bbba11e742f2ffed4fe63d9031
WangQing <2917021186@qq.com>
2026-04-10 01:45:54 +0000

    [ascend] remove l2normfwd

--- dlinfer/vendor/ascend/triton_ops/fla/__init__.py
+++ dlinfer/vendor/ascend/triton_ops/fla/__init__.py
@@ -2,10 +2,8 @@
 
 from .chunk import chunk_gated_delta_rule
 from .sigmoid_gating import fused_sigmoid_gating_delta_rule_update
-from triton_ascend_kernels.norm.l2norm import l2norm_fwd
 
 __all__ = [
     "chunk_gated_delta_rule",
-    "l2norm_fwd",
     "fused_sigmoid_gating_delta_rule_update",
 ]
